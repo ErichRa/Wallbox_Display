f2f6297c43856515fa4cbe4aab25f1c005d304cf
ErichRa <36544399+ErichRa@users.noreply.github.com>
2026-08-23 11:03:24 +0200

    Expose total energy footer label

--- include/ui.h
+++ include/ui.h
@@ -19,7 +19,7 @@ extern lv_obj_t *ui_SocLabel;
 extern lv_obj_t *ui_VoltageLabel;
 extern lv_obj_t *ui_EnergyTodayLabel;
 extern lv_obj_t *ui_ChargeTimeLabel;
-extern lv_obj_t *ui_TemperatureLabel;
+extern lv_obj_t *ui_TotalEnergyLabel;
 extern lv_obj_t *ui_WallboxOnlineLabel;
 extern lv_obj_t *ui_VehicleLabel;
 extern lv_obj_t *ui_WifiLabel;
