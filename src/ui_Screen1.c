7f4b6a44e4b32c9bc9f336eb947f1c8c67435134
ErichRa <36544399+ErichRa@users.noreply.github.com>
2026-08-23 20:41:39 +0200

    Move set-current control below current gauge

--- src/ui_Screen1.c
+++ src/ui_Screen1.c
@@ -59,12 +59,18 @@ void ui_Screen1_screen_init(void)
     make_label(ui_Screen1, "WALLBOX", 28, 18, &lv_font_montserrat_32, COLOR_TEXT);
     make_label(ui_Screen1, "Smart Charging", 30, 52, &lv_font_montserrat_16, COLOR_MUTED);
 
-    ui_WifiLabel = make_label(ui_Screen1, "WLAN  -- dBm", 430, 18,
+    ui_WifiLabel = make_label(ui_Screen1, "WLAN  -- dBm", 400, 18,
                               &lv_font_montserrat_16, COLOR_GREEN);
-    ui_MqttLabel = make_label(ui_Screen1, "MQTT  --", 575, 18,
+    lv_obj_set_width(ui_WifiLabel, 160);
+    lv_obj_set_style_text_align(ui_WifiLabel, LV_TEXT_ALIGN_RIGHT, 0);
+    ui_MqttLabel = make_label(ui_Screen1, "MQTT  --", 570, 18,
                               &lv_font_montserrat_16, COLOR_GREEN);
-    ui_IpLabel = make_label(ui_Screen1, "IP: ---.---.---.---", 575, 47,
+    lv_obj_set_width(ui_MqttLabel, 202);
+    lv_obj_set_style_text_align(ui_MqttLabel, LV_TEXT_ALIGN_RIGHT, 0);
+    ui_IpLabel = make_label(ui_Screen1, "IP: ---.---.---.---", 570, 47,
                             &lv_font_montserrat_14, COLOR_MUTED);
+    lv_obj_set_width(ui_IpLabel, 202);
+    lv_obj_set_style_text_align(ui_IpLabel, LV_TEXT_ALIGN_RIGHT, 0);
 
     lv_obj_t *header_line = lv_obj_create(ui_Screen1);
     lv_obj_set_pos(header_line, 24, 76);
@@ -77,7 +83,7 @@ void ui_Screen1_screen_init(void)
     lv_obj_t *main_panel = make_panel(ui_Screen1, 24, 90, 752, 230);
 
     ui_CurrentArc = lv_arc_create(main_panel);
-    lv_obj_set_pos(ui_CurrentArc, 20, 23);
+    lv_obj_set_pos(ui_CurrentArc, 20, 8);
     lv_obj_set_size(ui_CurrentArc, 185, 185);
     lv_arc_set_range(ui_CurrentArc, 0, 160);
     lv_arc_set_value(ui_CurrentArc, 0);
@@ -89,15 +95,15 @@ void ui_Screen1_screen_init(void)
     lv_obj_set_style_arc_width(ui_CurrentArc, 14, LV_PART_INDICATOR);
     lv_obj_set_style_bg_opa(ui_CurrentArc, LV_OPA_TRANSP, LV_PART_KNOB);
 
-    ui_ChargePhaseLabel = make_label(main_panel, "off", 20, 66,
+    ui_ChargePhaseLabel = make_label(main_panel, "off", 20, 51,
                                      &lv_font_montserrat_18, COLOR_GREEN);
     lv_obj_set_width(ui_ChargePhaseLabel, 185);
     lv_obj_set_style_text_align(ui_ChargePhaseLabel, LV_TEXT_ALIGN_CENTER, 0);
-    ui_CurrentLabel = make_label(main_panel, "--.- A", 20, 105,
+    ui_CurrentLabel = make_label(main_panel, "--.- A", 20, 90,
                                  &lv_font_montserrat_28, COLOR_TEXT);
     lv_obj_set_width(ui_CurrentLabel, 185);
     lv_obj_set_style_text_align(ui_CurrentLabel, LV_TEXT_ALIGN_CENTER, 0);
-    lv_obj_t *current_caption = make_label(main_panel, "LADESTROM", 20, 142,
+    lv_obj_t *current_caption = make_label(main_panel, "LADESTROM", 20, 127,
                                            &lv_font_montserrat_14, COLOR_MUTED);
     lv_obj_set_width(current_caption, 185);
     lv_obj_set_style_text_align(current_caption, LV_TEXT_ALIGN_CENTER, 0);
@@ -146,27 +152,29 @@ void ui_Screen1_screen_init(void)
     lv_obj_set_style_bg_opa(divider, LV_OPA_COVER, 0);
     lv_obj_set_style_border_width(divider, 0, 0);
 
-    make_label(main_panel, "Ladeenergie", 570, 12, &lv_font_montserrat_14, COLOR_YELLOW);
-    ui_EnergyTodayLabel = make_label(main_panel, "--.-- kWh", 570, 31,
+    make_label(main_panel, "Ladeenergie", 570, 20, &lv_font_montserrat_14, COLOR_YELLOW);
+    ui_EnergyTodayLabel = make_label(main_panel, "--.-- kWh", 570, 40,
                                      &lv_font_montserrat_20, COLOR_TEXT);
 
-    make_label(main_panel, "Ladezeit", 570, 59, &lv_font_montserrat_14, COLOR_MUTED);
-    ui_ChargeTimeLabel = make_label(main_panel, "--.-- h", 570, 78,
+    make_label(main_panel, "Ladezeit", 570, 72, &lv_font_montserrat_14, COLOR_MUTED);
+    ui_ChargeTimeLabel = make_label(main_panel, "--.-- h", 570, 92,
                                     &lv_font_montserrat_20, COLOR_TEXT);
 
-    make_label(main_panel, "Ladebeginn", 570, 108, &lv_font_montserrat_14, COLOR_MUTED);
-    ui_SessionStartLabel = make_label(main_panel, "---", 570, 124,
+    make_label(main_panel, "Ladebeginn", 570, 124, &lv_font_montserrat_14, COLOR_MUTED);
+    ui_SessionStartLabel = make_label(main_panel, "---", 570, 142,
                                       &lv_font_montserrat_14, COLOR_TEXT);
 
-    make_label(main_panel, "Ladeende", 570, 146, &lv_font_montserrat_14, COLOR_MUTED);
-    ui_SessionEndLabel = make_label(main_panel, "---", 570, 162,
+    make_label(main_panel, "Ladeende", 570, 170, &lv_font_montserrat_14, COLOR_MUTED);
+    ui_SessionEndLabel = make_label(main_panel, "---", 570, 188,
                                     &lv_font_montserrat_14, COLOR_TEXT);
 
-    ui_SetCurrentLabel = make_label(main_panel, "Soll: -- A", 570, 201,
+    ui_SetCurrentLabel = make_label(main_panel, "Soll: -- A", 25, 204,
                                     &lv_font_montserrat_14, COLOR_MUTED);
+    lv_obj_set_width(ui_SetCurrentLabel, 88);
+    lv_obj_set_style_text_align(ui_SetCurrentLabel, LV_TEXT_ALIGN_RIGHT, 0);
 
     ui_SetCurrentSlider = lv_slider_create(main_panel);
-    lv_obj_set_pos(ui_SetCurrentSlider, 655, 202);
+    lv_obj_set_pos(ui_SetCurrentSlider, 120, 207);
     lv_obj_set_size(ui_SetCurrentSlider, 82, 10);
     lv_slider_set_range(ui_SetCurrentSlider, 6, 16);
     lv_slider_set_value(ui_SetCurrentSlider, 6, LV_ANIM_OFF);
